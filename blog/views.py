350436b7db69ecf0325e9a9f74ebb209356f4dba
Yana <yanazvukova@gmail.com>
2023-10-15 19:07:20 +0300

    .

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post, Contest
 
@@ -11,3 +11,8 @@ def post_list(request):
 def contest_list(request):
     contests = Contest.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/contests_list.html', {'contests': contests})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
